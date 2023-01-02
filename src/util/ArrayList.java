76aa50c226d8e0cb1a113cceded09460b8a2ac4d
Slava Rybkin <yolka.sd@gmail.com>
2023-01-02 21:13:55 +0700

    Implemented the ArrayList class

--- src/util/ArrayList.java
+++ src/util/ArrayList.java
@@ -1,4 +1,29 @@
 package util;
 
-public class ArrayList implements List{
+public class ArrayList<E> implements List<E>{
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
