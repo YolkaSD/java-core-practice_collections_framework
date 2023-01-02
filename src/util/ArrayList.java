820c1ea05cdaba1b0e438ff88fa0eeed6eeae45b
Slava Rybkin <yolka.sd@gmail.com>
2023-01-02 22:26:31 +0700

    Added two constructors: ArrayList() and ArrayList(int initialCapacity)

--- src/util/ArrayList.java
+++ src/util/ArrayList.java
@@ -1,6 +1,35 @@
 package util;
 
 public class ArrayList<E> implements List<E>{
+    //The field responsible for the size of the dynamic array be default
+    private static final int DEFAULT_CAPACITY = 10;
+
+    //The field that stores all elements of the collection
+    transient Object[] elementData;
+
+    //A counter field that stores the number of elements actually in the array
+    private int size;
+
+    //
+    private static final Object[] EMPTY_ELEMENTDATA = {};
+
+    public ArrayList() {
+        this.elementData = EMPTY_ELEMENTDATA;
+    }
+
+    //Constructs an empty list with the specified initial capacity.
+    //Parameter: initialCapacity - the initial capacity of the list from 0 to java.lang.Integer.MAX_VALUE
+    //Throws: IllegalArgumentException – if the specified initial capacity is negative
+    public ArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.elementData = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.elementData = EMPTY_ELEMENTDATA;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
+
+    }
 
     @Override
     public boolean add(E e) {
